80f647ce25b68a030e31777705996b14f06c74d0
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-02 21:55:41 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     elif n == 0:
         print("0 arguments.")
     else:
-        print("{} arguments".format(n))
+        print("{} arguments:".format(n))
     for i in range(1, n + 1):
         print("{}: {}".format(i, sys.argv[i]))
     
