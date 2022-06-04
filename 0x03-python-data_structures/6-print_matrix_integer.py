9157b1a2dab4a85ba52327e4c5420b6392f9b41d
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-04 22:58:00 +0100

    Create 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    n = len(matrix)
+    for i in range(0, n):
+        y = len(matrix[i])
+        for z in range(0, y):
+            print("{}".format(matrix[i][j]), end = " ")
+     print("")
