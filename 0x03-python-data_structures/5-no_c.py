b71a88134ce4ebfe8d0dcc6c0a8edea3593b839d
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-04 22:27:56 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     resuilt = ""
-    for i in string:
+    for i in my_string:
         if i == 'c' or i == 'C':
             continue
         resuilt += i
