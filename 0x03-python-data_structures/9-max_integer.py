a930d40811008317f78eeb91a61923ad487025c1
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-05 18:06:55 +0100

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
         return None
     else:
         for i in range(0, n):
-            for j in range(i + 1, n):
+            for j in range(i, n):
                 if my_list[i] < my_list[j]:
                     break
                 if (j == n - 1):
