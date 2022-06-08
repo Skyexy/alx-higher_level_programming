259eb87328b9f9a01f9e577d97b84bcb74273092
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-08 23:08:49 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def func(x):
     return x * x
+
+
 def square_matrix_simple(matrix=[]):
     n = len(matrix)
     listf = []
-    for  i in range(0, n):
+    for i in range(0, n):
         r = list(map(func, matrix[i]))
         listf.append(r)
     return listf
