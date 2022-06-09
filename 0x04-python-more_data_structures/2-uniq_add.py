a50d4a9bcd64b44fccf112e46b97b783d563a3a0
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-09 14:09:03 +0100

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -8,7 +8,7 @@ def uniq_add(my_list=[]):
             if z == d:
                 m = 1
                 break
-            mem.append(z)
+        mem.append(z)
         if m == 0:
             i = i + z
     return i
