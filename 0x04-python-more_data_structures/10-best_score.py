a71a3dd31fa5666041d001685a7b553ca582b642
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-09 18:28:31 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    new_values = a_dictionary.items()
-    dic = {}
-    m = " "
-    z = 0
-    for k, y in new_values:
-        if z < y:
-            z = y
-            m = k
-    return m
+def best_score(my_dict):
+    return max(my_dict, key=my_dict.get) if my_dict else None
