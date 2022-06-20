7c8d225d6814b5d791c314a279f54d1b65b36a3a
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-20 22:53:39 +0100

    Create 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    y = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(i), end="")
+            y += 1
+        except (ValueError, IndexError):
+            pass
+    print("")
+    return y
