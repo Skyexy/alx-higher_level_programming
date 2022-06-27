f410b1b675446293ade5c4ca7d187186421907f7
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-27 16:33:46 +0100

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,22 +10,22 @@ def matrix_divided(matrix, div):
     round1 = 0
     if type (div) != (int or float):
         raise TypeError("div must be a number")
-        if div == 0:
+    if div == 0:
             raise ZeroDivisionError("division by zero")
-        if type(matrix) == list:
-            for row in matrix:
-                if type(row) != list:
-                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-                if len(row) == len(matrix[1]):
-                    for i in row:
-                        if type(i) != (int or float):
-                            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(matrix) == list:
+        for row in matrix:
+            if type(row) != list:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            if len(row) == len(matrix[1]):
+                for i in row:
+                    if type(i) != (int or float):
+                        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
                         else:
                             list2.append(round(i/div, 2))
                             listl.append(list2.copy())
                             list2.clear()
-                else:
-                    raise TypeError("Each row of the matrix must have the same size")
+            else:
+                raise TypeError("Each row of the matrix must have the same size")
     else:
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     return listl
