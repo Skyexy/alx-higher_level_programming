cbefe03119634c848d121d372307e3755a8b5dc3
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-27 14:29:21 +0100

    Create 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+"""
+a function that adds 2 integers
+"""
+
+def add_integer(a, b=98):
+    """a function that adds 2 integers
+    """
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    elif type(b) != int and type(b) != float:
+        raise TypeError("a must be an integer")
+    if type(a) == float:
+        a = int(a)
+    elif type(b) == float:
+        b = int(b)
+    return (a + b)
