cd054fc63421efd5c73d2753da59e75747c8bf15
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-28 12:26:13 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 """
-a function that adds 2 integers
+Module 0-add_integer
+contain one method and accept two values of int or float type
+and cast them to int  before adding
+Return an int sum
 """
 
+
 def add_integer(a, b=98):
-    """a function that adds 2 integers
     """
-    if type(a) != int and type(a) != float:
+        Return the summation  of two argument
+    """
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    elif type(b) != int and type(b) != float:
-        raise TypeError("a must be an integer")
-    if type(a) == float:
-        a = int(a)
-    elif type(b) == float:
-        b = int(b)
-    return (a + b)
+    elif type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    else:
+        if type(a) is float:
+            a = int(a)
+        if type(b) is float:
+            b = int(b)
+        return a + b
