2a2cdb473c3d29a51002786c4573a6894cf7f947
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-07-10 17:58:57 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,24 +1,42 @@
 #!/usr/bin/python3
+"""
+    contains class Square implements class Rectangle
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+        Square implements rectangle
+    """
     def __init__(self, size, x=0, y=0, id=None):
+        """
+            initialises Square (overrides Rectangle init)
+        """
         super().__init__(size, size, x, y, id)
         self.__size = size
         self.__x = x
         self.__y = y
 
     def __str__(self):
+        """
+            returns the size of the square
+        """
         return ("[{}] ({}) {}/{} - {}"
                 .format(type(self).__name__, self.id, self.__x, self.__y, self.__size))
 
     @property
     def size(self):
+        """
+            returns the size of the square
+        """
         return self.__size
 
     @size.setter
     def size(self, size):
+        """
+            sets the value of size
+        """
         if type(size) != int:
             raise TypeError("width must be an integer")
         elif size <= 0:
@@ -28,6 +46,13 @@ class Square(Rectangle):
         self.__size = size
 
     def update(self, *args, **kwargs):
+        """
+            assigns key/value argument to attributes
+            kwargs is skipped if args is not empty
+            Args:
+                *args -  variable number of no-keyword args
+                **kwargs - variable number of keyworded args
+        """
         if args:
             try:
                 self.id = args[0]
@@ -43,5 +68,8 @@ class Square(Rectangle):
             self.__y = kwargs.get('y', self.__y)
 
     def to_dictionary(self):
+        """
+            Returns the dictionary representation of a Square
+        """
         s = {'id': self.id, 'x': self.__x, 'size': self.__size, 'y': self.__y}
         return s
