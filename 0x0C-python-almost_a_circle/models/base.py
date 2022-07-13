4ece179af668fb2e2e724060dcb39996e66209de
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-07-13 11:20:15 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,18 +17,18 @@ class Base:
     """
 
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         """
            Initializes the class attributes.
            Args:
                id (int)
         """
-        if id is not None:
-            self.id = id
-        else:
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+        else:
+            self.id = id
 
     @staticmethod
     def to_json_string(list_dictionaries):
@@ -37,26 +37,7 @@ class Base:
         """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """
-            writes the JSON strin representation of list_objs to a file
-        """
-        filename = "{}.json".format(cls.__name__)
-        list1 = []
-
-        if list_objs is None:
-            with open(filename, "w") as f:
-                f.write("[]")
-        for i in list_objs:
-            dic = i.to_dictionary()
-            list1.append(dic)
-        js = json.loads(cls.to_json_string(list1))
-        with open(filename, "w", encoding="utf-8") as f:
-            json.dump(js, f)
+        return json.dumps(list_dictionaries)
 
     @staticmethod
     def from_json_string(json_string):
@@ -64,74 +45,99 @@ class Base:
             returns the list of the JSON string representation
         """
         if json_string is None or len(json_string) == 0:
-            return "[]"
-        else:
-            return json.loads(json_string)
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+            writes the JSON strin representation of list_objs to a file
+        """
+        fname = cls.__name__ + ".json"
+        content = []
+
+        if list_objs is not None:
+            for obj in list_objs:
+                obj = obj.to_dictionary()
+                json_dict = json.loads(cls.to_json_string(obj))
+                content.append(json_dict)
+
+        with open(fname, "w") as jfile:
+            json.dump(content, jfile)
 
     @classmethod
     def create(cls, **dictionary):
         """
             returns an instance with all attributes already set.
         """
-        from rectangle import Rectangle
-        from square import Square
-        if cls.__name__ == 'Rectangle':
-            m = Rectangle(2, 3)
-        if cls.__name__ == 'Square':
-            m = Square(4)
-        m.update(**dictionary)
-        return m
+        from models.rectangle import Rectangle
+        from models.square import Square
+
+        if cls.__name__ == "Rectangle":
+            mod = Rectangle(2, 7)
+        elif cls.__name__ == "Square":
+            mod = Square(6)
+        mod.update(**dictionary)
+        return (mod)
 
     @classmethod
     def load_from_file(cls):
         """
             returns a list of instances
         """
-        filename = "{}.json".format(cls.__name__)
-        list1 = []
+        fname = cls.__name__ + ".json"
+
         try:
-            with open(filename, "r", encoding="utf-8") as f:
-                list2 = cls.from_json_string(f.read())
+            with open(fname, encoding='utf8') as jfile:
+                content = cls.from_json_string(jfile.read())
         except:
-            return list
-        for i in list2:
-            list1.append(cls.create(**i))
-        return list1
+            return []
+
+        instances = []
+
+        for instance in content:
+            temp = cls.create(**instance)
+            instances.append(temp)
+
+        return instances
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
         """
             serializes in CSV and saves in a file
         """
-        filename = "{}.csv".format(cls.__name__)
+        fname = cls.__name__ + ".csv"
 
         if list_objs is None:
-            with open(filename, "w") as f:
-                f.write("[]")
-        with open(filename, "w") as f:
-            writer = csv.writer(f)
-            for i in list_objs:
-                if cls.__name__ == 'Rectangle':
-                    writer.writerow([i.id, i.width, i.height, i.x, i.y])
-                if cls.__name__ == 'Square':
-                    writer.writerow([i.id, i.size, i.x, i.y])
+            with open(fname, "w") as cfile:
+                cfile.write("[]")
+        else:
+            with open(fname, "w") as cfile:
+                writer = csv.writer(cfile)
+                for obj in list_objs:
+                    if cls.__name__ == "Rectangle":
+                        writer.writerow([obj.id, obj.width, obj.height, obj.x, obj.y])
+                    if cls.__name__ == "Square":
+                        writer.writerow([obj.id, obj.width, obj.x, obj.y])
 
     @classmethod
     def load_from_file_csv(cls):
         """
             deserializes from CSV from a file.
         """
-        filename = "{}.csv".format(cls.__name__)
-        with open(filename, "r") as f:
-            if cls.__name__ == 'Rectangle':
-                red = csv.DictReader(f, fieldnames=['id', 'width', 'height', 'x', 'y'])
+        fname = cls.__name__ + ".csv"
 
-            elif cls.__name__ == 'Square':
-                red = csv.DictReader(f, fieldnames=['id', 'size', 'x', 'y'])
+        with open(fname, "r") as cfile:
+            if cls.__name__ == "Rectangle":
+               reader = csv.DictReader(cfile, fieldnames={'id','width',
+                                                          'height', 'x', 'y'})
+            elif cls.__name__ == "Square":
+               reader = csv.DictReader(cfile, fieldnames={'id', 'size', 'x', 'y'})
 
-            list1 = []
-            for instance in red:
-                list2 = {x: int(y) for x, y in instance.items()}
-                temp = cls.create(**list2)
-                list1.append(temp)
-        return list1
+            instances = []
+            for instance in reader:
+                instance = {x: int(y) for x, y in instance.items()}
+                temp = cls.create(**instance)
+                instances.append(temp)
+
+        return instances
