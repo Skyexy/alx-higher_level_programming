86323019c9451e631af7e452802a8730b9fcd868
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-08-09 22:40:26 +0100

    Create 5-full_table.sql

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- full description of the table first_table from the database hbtn_0c_0 in your MySQL server
+-- db name will be passed as argument of mysql cmd
+-- Not allowed to use DESCRIBE or EXPLAIN
+SHOW COLUMNS FROM first_table;
