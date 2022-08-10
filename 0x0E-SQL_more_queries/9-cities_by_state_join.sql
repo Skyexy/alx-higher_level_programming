21094644f5e0fdc68a35ed344057d679f71b62ad
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-08-10 19:47:34 +0100

    Create 9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- List all cities of CA that can be found in db 'hbtn_0d_usa'
+-- 'states' table contains only one record where 'name = California'
+-- but 'id' cant be different
+-- Results must be sorted in ascending order by 'cities.id'
+-- Not allowed to use JOIN keyword
+SELECT city.id, city.name, state.name
+FROM states
+  INNNER JOIN cities
+ON states.id = cities.state_id;
