47586ac1cd77817bcb616422a1766ea23739c1c0
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-06 15:51:47 +0100

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
 if (process.argv.length < 3) {
   console.log('No argument');
-} else {
-  process.argv.forEach((val, index) => {
-    if (index > 2) {
-      console.log(val);
-    }
-  });
 }else {
 	const args = process.argv.slice(2);
 	console.log(args);
