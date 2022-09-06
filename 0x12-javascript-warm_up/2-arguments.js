5a645cba507acf22077ba83b829acd92f6acb710
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-06 14:52:16 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-if (process.argv.length < 2)
+if (process.argv.length < 3)
 {
 	console.log('No argument');
 }
-else if (process.argv.length === 2)
+else if (process.argv.length === 3)
 {
-	console.log(process.argv);
 	console.log('Argument found');
 }
 else
