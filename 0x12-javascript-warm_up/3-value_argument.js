364d2479d95a70a2bc57d2fb7aea9e67d99d457b
Skyexy <oniprogress1234@gmail.com>
2022-09-06 14:56:08 +0000

    loop

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 if (process.argv.length < 3) {
   console.log('No argument');
-}else {
-	const args = process.argv.slice(2);
-	args.forEach((val, index) => {
-		console.log(val);
-	});
+} else {
+  const args = process.argv.slice(2);
+  args.forEach((val, index) => {
+    console.log(val);
+  });
 }
