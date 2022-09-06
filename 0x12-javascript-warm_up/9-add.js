f2ad5b75692ff28ccbc41bb652db6cec2f9d0d76
Skyexy <oniprogress1234@gmail.com>
2022-09-06 16:43:42 +0000

    js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-function add(a, b){
-	let z = a + b;
-	console.log(z);
+function add (a, b) {
+  const z = a + b;
+  console.log(z);
 }
 const args = parseInt(process.argv[2]);
 const args2 = parseInt(process.argv[3]);
