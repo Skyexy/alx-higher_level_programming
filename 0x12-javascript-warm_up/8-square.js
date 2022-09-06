7c6cce12d2923e99cd26fab9576eba6d36e78444
Skyexy <oniprogress1234@gmail.com>
2022-09-06 16:20:44 +0000

    js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const args = parseInt(process.argv[2]);
-if (args){
- for (let i = 0; i < args; i++) {
-  console.log('X'*args);
- }
-}
-else{
- console.log('Missing size');
+if (args) {
+  for (let i = 0; i < args; i++) {
+    console.log('X' * args);
+  }
+} else {
+  console.log('Missing size');
 }
