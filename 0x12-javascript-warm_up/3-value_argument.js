1c239a8a2f812d40c0c69a5cf9fe7f851572f9c1
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-06 15:59:31 +0100

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (process.argv.length < 3) {
+if (process.argv.length < 2) {
   console.log('No argument');
 } else {
-  console.log(process.argv[3]);
+  console.log(process.argv[2]);
 }
