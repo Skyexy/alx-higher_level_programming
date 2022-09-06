7a9ada6262ce3d708eb79a9ac33ad527d99ff331
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-06 17:03:44 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 const args = parseInt(process.argv[2]);
-for (let i = 0; i < args; i++) {
-  console.log('C is fun');
+if (args){
+  for (let i = 0; i < args; i++) {
+    console.log('C is fun');
+  }
+}
+else{
+  console.log('Missing number of occurrences');
 }
