44ea667b38808f470e82d95488b61bd6a2d7320e
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-06 17:50:54 +0100

    Create 10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let z = 1;
+const args = parseInt(process.argv[2]);
+if (args === 0 || args === 1 || args === NaN){
+    console.log(z);
+}
+else{
+	for(let i = args; i >= 1; i--){
+      z = z * i;
+    }
+    console.log(z);
+}
