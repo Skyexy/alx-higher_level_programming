16bccf49beec4a1a4b6590d2705b39fadb3b66ad
Skyexy <oniprogress1234@gmail.com>
2022-09-07 19:54:23 +0000

    loop

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 let args = 0;
 
-if (process.argv[3]){
-  let me = process.argv.slice(2);
-  me.sort(function(a, b){return b-a});
+if (process.argv[3]) {
+  const me = process.argv.slice(2);
+  me.sort(function (a, b) { return b - a; });
   args = me[1];
 }
 console.log(args);
