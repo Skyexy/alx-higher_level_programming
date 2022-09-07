7d8b2add36503948c839d085fb025b35261927d1
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-07 19:08:52 +0100

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 let args = 0;
+
 if (process.argv[3]){
-  args = process.argv.slice(2);
-  args.sort(function(a, b){return a - b});
-  args = args[1];
+  let me = process.argv.slice(2);
+  me.sort(function(a, b){return a - b});
+  me = me[1];
 }
 console.log(args);
