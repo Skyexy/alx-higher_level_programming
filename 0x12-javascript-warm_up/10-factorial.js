89a81d4c94fbd20ae2847b57e7bbfe3f59a72e29
Skyexy <oniprogress1234@gmail.com>
2022-09-07 17:43:49 +0000

    loop

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const args = parseInt(process.argv[2]);
-function factorial(n){
-  if(n === 0 || n === 1) {
+function factorial (n) {
+  if (n === 0 || n === 1) {
     return 1;
-  } else{
-    return n * factorial(n-1);
+  } else {
+    return n * factorial(n - 1);
   }
 }
 const z = factorial(args);
