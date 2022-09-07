9bf800945bd866d25bbae921f197310e1ba10766
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-08 00:53:17 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,17 +3,18 @@ const me = require('./5-square');
 module.exports = class Square extends me {
   constructor (size) {
     super(size);
+    this.size = size;
   }
   charPrint(c) {
     if (c) {
-      for (let i = 0; i < size; i++) {
-        console.log(c.repeat(size));
+      for (let i = 0; i < this.h; i++) {
+        console.log(c.repeat(this.h));
       }
     }
     else {
       const si = 'X';
-      for (let i = 0; i < size; i++) {
-        console.log(si.repeat(size));
+      for (let i = 0; i < this.h; i++) {
+        console.log(si.repeat(this.h));
       }
     }
   }
