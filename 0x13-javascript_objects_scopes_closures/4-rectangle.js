b6efa7f2207845505f26c440e754f60503624349
Skyexy <oniprogress1234@gmail.com>
2022-09-07 22:42:26 +0000

    js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -13,11 +13,13 @@ module.exports = class Rectangle {
       console.log(si.repeat(this.width));
     }
   }
+
   rotate () {
     const f = this.width;
     this.width = this.height;
     this.height = f;
   }
+
   double () {
     this.width = this.width * 2;
     this.height = this.height * 2;
