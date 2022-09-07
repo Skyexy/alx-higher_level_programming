eea026c088bd843687886bed4e6f2d544389d38b
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-07 23:29:41 +0100

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 1 || h > 1 || Number.isInteger(w) || Number.isInteger(h)) {
+    if (w > 1 && h > 1 && Number.isInteger(w) && Number.isInteger(h)) {
       this.width = w;
       this.height = h;
     }
