0b73cdd87573f3ebe14201fec7296538253a69ac
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-07 18:47:53 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-const args = parseInt(process.argv[2]);
+if (isNaN(process.argv[2])){
+  const args = 1;
+}
+else {
+  const args = parseInt(process.argv[2]);
+}
 function factorial (n) {
   if (n === 0 || n === 1) {
     return 1;
