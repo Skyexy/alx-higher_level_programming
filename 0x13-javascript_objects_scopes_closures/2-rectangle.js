2f3f638a907e3113bc83cd6fc1a60112f93eaa4e
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-07 22:22:26 +0100

    Create 2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+    if (w < 1 || h < 1){
+      var objectA = {};
+    }
+  }
+}
