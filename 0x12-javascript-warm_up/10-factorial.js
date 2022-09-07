5adfd35faa316d41ea3374d2be535c7fb54ba33d
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-07 18:37:36 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-let z = 1;
 const args = parseInt(process.argv[2]);
-if (args === 0 || args === 1) {
-  console.log(z);
-} else {
-  for (let i = args; i >= 1; i--) {
-    z = z * i;
+function factorial(n){
+  if(n == 0 || n == 1){
+    return 1;
+  }else{
+    return n * factorial(n-1);
   }
-  console.log(z);
+}
+console.log(factorial(args));
 }
