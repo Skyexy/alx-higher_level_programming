bba7bf5ed3bfe1a47f61670f5d37392a2316152e
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-09 14:46:38 +0100

    Create 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,6 @@
+import MySQLdb
+import sys
+
+db=MySQLdb.connect(host="localhost",user=sys.argv[1],port=3307,3306, passwd=sys.argv[2],db=sys.argv[3])
+r=db.store_result()
+print(r.fetch_row(maxrows=0))
