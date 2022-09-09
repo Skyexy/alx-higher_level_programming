60e74d6a8bcd73ec6f9e77e8776188255351f85a
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-09 15:39:42 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,15 +2,15 @@
 import sys
 from MySQLdb import _mysql
 
-mydb = _mysql.connect(
-  host= "localhost",
-  port = 3306,
-  user= sys.argv[1],
-  password= sys.argv[2],
-  database= sys.argv[3],
-)
-
-sql = """SELECT * FROM states ORDER BY id"""
-db.query(sql)
-r=db.store_result()
-print(r.fetch_row(maxrows=0))
+if __name__ == "__main__":
+	mydb = _mysql.connect(
+		host= "localhost",
+		port = 3306,
+		user= sys.argv[1],
+		password= sys.argv[2],
+		database= sys.argv[3],
+	)
+	sql = """SELECT id, name FROM states ORDER BY id ASC"""
+	db.query(sql)
+	r=db.store_result()
+	print(r.fetch_row(maxrows=0))
