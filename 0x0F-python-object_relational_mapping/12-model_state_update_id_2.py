6f862bef7aadc5025072ad2e501375fdef1d8b16
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-11 12:21:04 +0100

    Update 12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -9,11 +9,11 @@ from sqlalchemy import create_engine
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    res = session.query(State).get(2)
+	engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+						   .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+	Session = sessionmaker(bind=engine)
+	session = Session()
+	
+	res = session.query(State).get(2)
 	res.name = 'New Mexico'
-	session.commit(2)
+	session.commit()
