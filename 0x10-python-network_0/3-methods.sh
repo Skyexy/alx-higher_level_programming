ebcaea07fffea98c399a7428c5bd4a5214166c00
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-22 22:11:42 +0100

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL
-curl -sI "$1" | grep -i Allow | awk '{print $2}'
+curl -sI "$1" | grep -i Allow | awk '{ for (i=3; i<=NF; i++) printf("%s ",$i) }END{ print"" }'
