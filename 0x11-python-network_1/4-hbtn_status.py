e5a709d0dd27d2d0cc4a0aa1e5595dc98f68a883
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-23 22:34:56 +0100

    Create 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status"""
+import requests
+
+x = requests.get('https://alx-intranet.hbtn.io/status')
+print("Body response:")
+print("\t- type: {}".format(type(x)))
+print("\t- content: {}".format(x))
