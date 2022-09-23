4765b4f7a82e04fd7c580577c8a7be8d8930d0fb
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-23 22:06:55 +0100

    Create 1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status"""
+from urllib import request
+import sys
+
+if __name__ == "__main__":
+    with request.urlopen(sys.argv[1]) as res:
+        print (response.getheader("X-Request-Id"))
