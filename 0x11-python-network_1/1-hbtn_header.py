0f25de563a8d8565e886341642e6b5ede454e0ae
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-23 22:08:21 +0100

    Create 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,4 +5,4 @@ import sys
 
 if __name__ == "__main__":
     with request.urlopen(sys.argv[1]) as res:
-        print (response.getheader("X-Request-Id"))
+        print (res.getheader("X-Request-Id"))
