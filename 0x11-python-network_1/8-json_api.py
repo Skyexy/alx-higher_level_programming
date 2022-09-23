a0435415bc0a061192a9c9d964afc93bf14e3cd8
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-23 23:15:25 +0100

    Create 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    q = ""
+    if sys.argv[1]:
+        q = sys.argv[1]
+    args = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        args.raise_for_status()
+        json = args.json()
+        if len(json) == 0:
+            print("No result")
+        else:
+            print("[{:d}] {}".format(json['id'], json['name']))
+    except Exception:
+        print("Not a valid JSON")
