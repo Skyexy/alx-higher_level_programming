3cbf82a0e0512d4d16907dd7099ed7c15796e19a
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-18 23:05:19 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
 // a script that reads and prints the content of a file;
 var fs = require('fs');
-
-function asyncReadFile(filename){
-	try {
-		const contents = fs.readFile(filename, 'utf-8');
-
-		console.log(contents);
-	} catch (err) {
-		console.log(err);
-	}
-}
-
 const myArgs = process.argv.slice(2);
-asyncReadFile(myArgs[0]);
+
+fs.readFile(myArgs[0], 'utf8', function(err, data){
+	console.log(data);
+});
