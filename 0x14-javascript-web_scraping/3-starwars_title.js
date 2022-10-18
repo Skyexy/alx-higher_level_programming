3316eb4035404dad71e05694845e9b8502ed492b
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-19 00:22:56 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,6 +3,6 @@
 const myArgs = process.argv.slice(2);
 const request = require('request');
 
-request.get("https://swapi-api.hbtn.io/api/films/1/", function (err, res, data) {
-  console.log(data.title);
+request.get("https://swapi-api.hbtn.io/api/films/1/", function (err, res, body) {
+  console.log(body);
 });
