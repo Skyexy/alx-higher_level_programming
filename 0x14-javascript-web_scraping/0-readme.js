b3858d5f569c16697bb45813659c74a4a3d9c86c
Your Name <you@example.com>
2022-10-18 22:11:01 +0000

    loop

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 // a script that reads and prints the content of a file;
-var fs = require('fs');
+const fs = require('fs');
 const myArgs = process.argv.slice(2);
 
-fs.readFile(myArgs[0], 'utf8', function(err, data){
-	if (err) {
-		console.error(err);
-	}
-	else {
-		console.log(data);
-	}
+fs.readFile(myArgs[0], 'utf8', function (err, data) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
