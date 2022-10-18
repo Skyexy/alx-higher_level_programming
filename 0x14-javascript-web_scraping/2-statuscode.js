dc1386d1cc14eeaa70c776af71e4aed91dd2e0f8
Your Name <you@example.com>
2022-10-18 22:46:29 +0000

    loop

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // a script that display the status code of a GET request;
 const myArgs = process.argv.slice(2);
-var request=require('request');
+const request = require('request');
 
-request.get(myArgs[0], function (err,res,body) {
-  console.log("code: ", res.statusCode);
+request.get(myArgs[0], function (err, res, body) {
+  console.log('code: ', res.statusCode);
 });
