6f15187933b7c0ca1f745609577fa7a8f3a285ea
Your Name <you@example.com>
2022-10-19 21:42:52 +0000

    llook

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,14 +6,13 @@ const link = myArgs[0];
 
 request.get(link, { json: true }, (err, res, body) => {
   if (err) throw err;
-  var fs = {};
+  const fs = {};
   for (const resu of body) {
     if (resu.completed) {
       if (fs[resu.userId]) {
-          fs[resu.userId]++
-      }
-      else {
-        fs[resu.userId] = 1
+        fs[resu.userId]++;
+      } else {
+        fs[resu.userId] = 1;
       }
     }
   }
