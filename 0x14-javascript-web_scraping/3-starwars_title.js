ad3cfc8bde1eb38c88513b809ac353696cd5dc0a
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-19 21:42:33 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,14 +2,8 @@
 // a script that display the status code of a GET request;
 const myArgs = process.argv.slice(2);
 const request = require('request');
-const link = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
-const count = 0;
+var link = "https://swapi-api.hbtn.io/api/films/" + myArgs[0]
 
 request.get(link, { json: true }, (err, res, body) => {
-  for (const character of body.title) {
-    if (character.includes('18')) {
-      count++;
-    }
-  }
-  console.log(count);
+  console.log(body.title);
 });
