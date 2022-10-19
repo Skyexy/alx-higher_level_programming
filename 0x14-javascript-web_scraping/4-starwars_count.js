8a012e0a9caca32ec6792420e755e1d3687f098f
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-19 21:46:21 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // a script that display the status code of a GET request;
 const request = require('request');
-const link = process.argv[2];;
+const link = process.argv[2];
 const count = 0;
 
 request.get(link, { json: true }, (err, res, body) => {
