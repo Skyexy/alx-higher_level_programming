610c9cfea57bde8d0f5207a0f7b442cc210f7aa6
Your Name <you@example.com>
2022-10-19 21:20:09 +0000

    loop

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 // a script that display the status code of a GET request;
 const myArgs = process.argv.slice(2);
 const request = require('request');
-var link = "https://swapi-api.hbtn.io/api/films/" + myArgs[0]
+const link = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
 
 request.get(link, { json: true }, (err, res, body) => {
   if (err) throw err;
