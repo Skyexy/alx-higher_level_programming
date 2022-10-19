807a5c0f93a3ac0602436907609eff75dc4b454f
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-19 22:41:54 +0100

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // a script that display the status code of a GET request;
 const myArgs = process.argv.slice(2);
-const fs = {};
 const request = require('request');
 const link = myArgs[0];
 
-request.get(link, (err, res, body) => {
+request.get(link, { json: true }, (err, res, body) => {
   if (err) throw err;
+  var fs = {};
   for (const resu of body) {
     if (resu.completed) {
       if (fs[resu.userId]) {
