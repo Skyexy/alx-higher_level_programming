f2ec77e0af28fe9ff5f0bbc421e7aeb9911e6605
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-19 21:52:20 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const myArgs = process.argv.slice(2);
 const link = myArgs[0]
-const count = 0;
+var count = 0;
 
 request.get(link, { json: true }, (err, res, body) => {
   for (const resu of body.results) {
