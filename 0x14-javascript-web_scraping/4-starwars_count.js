5ed48c0bcf581b3cb5437b0a3f7794fa279c884d
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-19 01:15:58 +0100

    Create 4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// a script that display the status code of a GET request;
+const myArgs = process.argv.slice(2);
+const request = require('request');
+var link = "https://swapi-api.hbtn.io/api/people/18/"
+
+request.get( link, { json: true }, (err,res,body) => {
+  var bod = body.films;
+  console.log(bod.length);
+});
