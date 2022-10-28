74059484cc247c39a3870a2888c87f4704bf24d3
Your Name <you@example.com>
2022-10-28 14:34:43 -0700

    look

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,8 +1,7 @@
-$("DIV#toggle_header").click(function(){
-    if ($('header').is('.green')){
-        $("header").removeClass('green').addClass('red');
-    }
-    else {
-        $("header").removeClass('red').addClass('green');
-    }
+$('DIV#toggle_header').click(function () {
+  if ($('header').is('.green')) {
+    $('header').removeClass('green').addClass('red');
+  } else {
+    $('header').removeClass('red').addClass('green');
+  }
 });
--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,6 +1,5 @@
-$.getJSON("https://swapi-api.hbtn.io/api/films/?format=json", function (data, textStatus, jqXHR){
-    for(var i=0;i<data.count;i++)
-    {
-        $("UL#list_movies").append("<li>" + data.results[i].title + "</li>");
-    }
+$.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function (data, textStatus, jqXHR) {
+  for (let i = 0; i < data.count; i++) {
+    $('UL#list_movies').append('<li>' + data.results[i].title + '</li>');
+  }
 });
